01c042caf1730e821e273c1c99f925713eb37865
GAZOCAR5 <119977721+GAZOCAR5@users.noreply.github.com>
2024-12-01 22:26:45 -0300

    Update spinlock.h
    tarea 4

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,9 +1,10 @@
 // Mutual exclusion lock.
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
 struct spinlock {
-  uint locked;       // Is the lock held?
-
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
+    uint locked;       // ¿Está bloqueado el spinlock?
+    char *name;        // Nombre del lock
+    struct cpu *cpu;   // CPU que tiene el lock
+    uint pcs[10];      // Call stack para depuración
 };
-
+#endif // SPINLOCK_H
